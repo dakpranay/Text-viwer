ba4ac1ee76ea24af05e96ba3c74662bab8c20121
dakpranay <pranay.12019353@lpu.in>
2023-07-22 17:52:02 +0530

    add copy funcnality to copy the text area using react

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -11,6 +11,15 @@ export default function Textform(props) {
         setText(text.toUpperCase())
     }
 
+    const handelClClick = ()=>{
+        setText("")
+    }
+
+    const handlecopytext = ()=>{
+        navigator.clipboard.writeText(text)
+        
+    }
+
     const handelloClick = ()=>{
         setText(text.toLowerCase())
     }
@@ -28,7 +37,9 @@ export default function Textform(props) {
                 <textarea className="form-control" value={text} onChange={handelOnChange} id="mybox" rows="10"></textarea>
             </div >
             <button className="btn btn-primary mx-3" onClick={handelUpClick}>Convert to uppercase</button>
-            <button className="btn btn-primary" onClick={handelloClick}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-3" onClick={handelloClick}>Convert to lowercase</button>
+            <button className="btn btn-primary mx-3" onClick={handelClClick}>Clear TextArea</button>
+            <button className="btn btn-primary " onClick={handlecopytext}>Copy Text</button>
         </div>
         <div className="container my-3">
             <h1>Your text Summery</h1>
@@ -36,6 +47,7 @@ export default function Textform(props) {
             <p>{(0.008*a).toFixed(3)} Minutes rquired to read the text</p>
             <h2>Preview</h2>
             <p>{text}</p>
+            <p></p>
         </div>
         </>
     )
